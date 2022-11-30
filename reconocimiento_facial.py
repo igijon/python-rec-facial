16d721cbe115ccc221db8a6fbfda727376f57d7b
Inma Gijón <igijoninf@gmail.com>
2022-11-30 11:36:53 +0100

    Cargar imágenes

--- reconocimiento_facial.py
+++ reconocimiento_facial.py
@@ -1,2 +1,17 @@
-from cv2 import *
-import face_recognition  as fr
+from pathlib import Path
+import cv2
+import face_recognition as fr
+
+# Cargar imagenes
+foto_control = fr.load_image_file(Path('fotos', 'foto1.jpg'))
+foto_prueba = fr.load_image_file(Path('fotos', 'foto2.jpg'))
+
+foto_control = cv2.cvtColor(foto_control, cv2.COLOR_BGR2RGB)
+foto_prueba = cv2.cvtColor(foto_prueba, cv2.COLOR_BGR2RGB)
+
+# Mostrar imágenes
+cv2.imshow('Foto Control', foto_control)
+cv2.imshow('Foto Prueba', foto_prueba)
+
+# Mantener el programa abierto
+cv2.waitKey(0)
